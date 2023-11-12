cc3484bb3b18086936999f40fe19d877a2ca9cce
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-12 23:57:55 +0530

    complete functionality for submit route for form submissions

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
-
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
+// for static data to be accessible
+app.use(express.static("public"));
 
 // Setting up body-parser middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,14 +19,18 @@ app.get("/", (req, res) => {
 // Handling the submit route for form submissions
 app.post("/submit", (req, res) => {
     //Step 2 - Make the generate name functionality work
-    //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-    //Then:
-    //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-    //scroll down to see the two arrays.
-    //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-    //3. Test to make sure that the random words display in the h1 element in index.ejs
 
-    //TODO: Implement the logic to generate a random name and render index.ejs with the generated name
+    //IT should randomly pick an adjective from the const "adj" and a noun from const "noun",
+    const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+    const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+    
+    // Send the index.ejs as a response and add the adjective and noun to the res.render
+    res.render("index.ejs", {
+        adjective: randomAdj,
+        noun: randomNoun,
+      });
+
+    
 });
 
 
